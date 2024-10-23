4433794296c02ec7d403c06c8aef31cec187d446
AbhinavGupta <bengupta786@gmail.com>
2024-10-24 02:52:04 +0530

    add register controller and bug fixes

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,9 @@
 const asyncHandler=(requestHandler)=>{
-    (req,res,next)=>{
-        Promise.resolve(requestHandler(res,req,next)).catch((err)=>next(err))
+  return (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
     }
-
 }
-export {asyncHandler}
+export { asyncHandler }
 
 // const asyncHandler=(fn)=>async (req,res,next)=>{
 //     try {
@@ -15,3 +14,4 @@ export {asyncHandler}
 //         message:err.message
 //       })  
 //     }}
+//     export {asyncHandler}
